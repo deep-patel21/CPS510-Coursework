e5c24862b5bb785178bfde9807b48aa247c5be8f
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-09-22 09:00:57 -0400

    Update Sec09_A17_Combined_SQL.sql

--- Sec09_A17_Combined_SQL.sql
+++ Sec09_A17_Combined_SQL.sql
@@ -1,3 +1,6 @@
+-------------------------------
+-- Create Tables of Entities --
+-------------------------------
 CREATE TABLE Film (
   film_id NUMBER PRIMARY KEY,
   title VARCHAR2(100) NOT NULL,
@@ -53,3 +56,71 @@ CREATE TABLE Awards (
   winner_type VARCHAR2(20) CHECK (winner_type IN ('Person', 'Film')),
   FOREIGN KEY (winner_id, winner_type) REFERENCES PersonFilm(person_film_id, person_film_type)
 );
+
+------------------------------------
+-- Create Tables of Relationships --
+------------------------------------
+
+CREATE TABLE Actor_Acts_In_Film (
+  actor_id NUMBER NOT NULL, 
+  film_id NUMBER NOT NULL,
+  PRIMARY KEY (actor_id, film_id),
+  FOREIGN KEY (actor_id) REFERENCES Actor(actor_id),
+  FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+CREATE TABLE Director_Directs_Film (
+    director_id NUMBER NOT NULL,
+    film_id NUMBER NOT NULL,
+    PRIMARY KEY (director_id, film_id),
+    FOREIGN KEY (director_id) REFERENCES Director(director_id),
+    FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+CREATE TABLE Producer_Produces_Film (
+    producer_id NUMBER NOT NULL,
+    film_id NUMBER NOT NULL,
+    PRIMARY KEY (producer_id, film_id),
+    FOREIGN KEY (producer_id) REFERENCES Producer(producer_id),
+    FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+CREATE TABLE Studio_Owns_Film (
+    studio_id NUMBER NOT NULL,
+    film_id NUMBER NOT NULL,
+    PRIMARY KEY (studio_id, film_id),
+    FOREIGN KEY (studio_id) REFERENCES Studio(studio_id),
+    FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+CREATE TABLE Awards_Given_To_Film (
+    award_id NUMBER NOT NULL,
+    film_id NUMBER NOT NULL,
+    PRIMARY KEY (award_id, film_id),
+    FOREIGN KEY (award_id) REFERENCES Awards(award_id),
+    FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+CREATE TABLE User_Creates_Review (
+    user_id NUMBER NOT NULL,
+    review_id NUMBER NOT NULL,
+    PRIMARY KEY (user_id, review_id),
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (review_id) REFERENCES Review(review_id)
+);
+
+CREATE TABLE Review_Rates_Film (
+    review_id NUMBER NOT NULL,
+    film_id NUMBER NOT NULL,
+    rating NUMBER,
+    PRIMARY KEY (review_id, film_id),
+    FOREIGN KEY (review_id) REFERENCES Review(review_id),
+    FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+
+
+
+
+
+
