cbe83a06c84003fd06e3f6565315659c81ce9b6a
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-09-22 09:37:07 -0400

    Update Sec09_A17_Combined_SQL.sql

--- Sec09_A17_Combined_SQL.sql
+++ Sec09_A17_Combined_SQL.sql
@@ -118,6 +118,73 @@ CREATE TABLE Review_Rates_Film (
     FOREIGN KEY (film_id) REFERENCES Film(film_id)
 );
 
+----------------------------
+-- Update Values in Table --
+----------------------------
+
+-- Insert Details into Movie Table
+INSERT INTO Movie (movie_id, title, runtime, release_year, director_id, cast)
+VALUES (1, 'Oppenheimer', 180, 2023, 1, 'Cillian Murphy, Robert Downey Jr., Matt Damon');
+
+INSERT INTO Movie (movie_id, title, runtime, release_year, director_id, cast)
+VALUES (2, 'Terminator 2: Judgement Day', 137, 1991, 2, 'Arnold Schwarznegger, Linda Hamilton, Edward Furlong');
+
+INSERT INTO Movie (movie_id, title, runtime, release_year, director_id, cast)
+VALUES (3, 'Mission: Impossible - Ghost Protocol', 172, 2011, 3, 'Tom Cruise, Jeremy Renner, Simon Pegg');
+
+-- Insert Details into Actor Table
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (1, 'Cillian', 'Murphy', TO_DATE('1976-05-25', 'YYYY-MM-DD'), 'Irish', '28 Days Later, The Dark Night, Oppenheimer');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (2, 'Robert', 'Downey Jr.', TO_DATE('1965-04-04', 'YYYY-MM-DD'), 'American', 'Iron Man 3, Sherlock Holmes, Avengers: Endgame');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (3, 'Matt', 'Damon', TO_DATE('1970-10-08', 'YYYY-MM-DD'), 'American', 'The Martian, Good Will, Hunting, Ford v Ferrari');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (4, 'Arnold', 'Schwarzenegger', TO_DATE('1947-07-30', 'YYYY-MM-DD'), 'Austrian-American', 'Terminator 2: Judgement Day, The 6th Day, Last Action Hero');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (5, 'Linda', 'Hamilton', TO_DATE('1956-09-26', 'YYYY-MM-DD'), 'American', 'Terminator: Dark Fate, Easy Does It, Lost Girl');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (6, 'Edward', 'Furlong', TO_DATE('1977-08-02', 'YYYY-MM-DD'), 'American', 'Terminator 2: Judgement Day, The Green Hornet, Detroit Rock City');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (7, 'Tom', 'Cruise', TO_DATE('1962-07-03', 'YYYY-MM-DD'), 'American', 'Top Gun: Maverick, Mission: Impossible - Dead Reckoning Part One, Jack Reacher');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (8, 'Jeremy', 'Renner', TO_DATE('1971-01-07', 'YYYY-MM-DD'), 'American', 'Captain America: Civil War, Kill the Messenger, Mission: Impossible - Rogue Nation');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (9, 'Simon', 'Pegg', TO_DATE('1970-02-14', 'YYYY-MM-DD'), 'British', 'The Ice Age Adventures of Buck Wild, Hot Fuzz, Mission: Impossible - Dead Reckoning Part One');
+
+
+-- Insert Details into Director Table
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (1, 'Christopher', 'Nolan', TO_DATE('1970-07-30', 'YYYY-MM-DD'), 'British-American', 'Tenet, Interstellar, Inception');
+
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (2, 'James', 'Cameron', TO_DATE('1954-08-16', 'YYYY-MM-DD'), 'Canadian-American', 'Titanic, Avatatr: The Way of Water, Terminator 2: Judgement Day');
+
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (3, 'Brad', 'Bird', TO_DATE('1957-09-24', 'YYYY-MM-DD'), 'American', 'The Incredibles, Ratatouille, Cars');
+
+-- Insert Details into Producer Table
+INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (1, 'Emma', 'Thomas', TO_DATE('1971-12-09', 'YYYY-MM-DD'), 'British', 'Inception, Interstellar, Dunkirk');
+
+INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (2, 'Edward', 'Furlong', TO_DATE('1977-08-02', 'YYYY-MM-DD'), 'American', 'Living & Dying, Heart of a Champion, Matt''s Chance');
+
+INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (3, 'Bryan', 'Burk', TO_DATE('1968-12-30', 'YYYY-MM-DD'), 'American', 'Mission: Impossible - Rogue Nation, Star Trek Into Darkness, The Cloverfield Paradox');
+
+
+
+
+
 
 
 
