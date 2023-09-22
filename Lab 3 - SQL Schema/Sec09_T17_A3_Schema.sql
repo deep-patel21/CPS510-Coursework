112e1d8afc2664619847666321140d98b9200197
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-09-22 08:44:43 -0400

    Update Sec09_T17_A3_Schema.sql

--- Lab 3 - SQL Schema/Sec09_T17_A3_Schema.sql
+++ Lab 3 - SQL Schema/Sec09_T17_A3_Schema.sql
@@ -1,7 +1,55 @@
 CREATE TABLE Film (
-  first_name VARCHAR2(50) NOT NULL,
+  film_id NUMBER PRIMARY KEY,
+  title VARCHAR2(100) NOT NULL,
+  runtime NUMBER,
+  release_year NUMBER, 
+  director_id NUMBER,
+  FOREIGN KEY (director_id) REFERENCES Director(director_id),
+  cast VARCHAR2(1000)
+);
+
+CREATE TABLE Actor (
+  actor_id NUMBER PRIMARY KEY,
+  first_name VARCHAR2(50) NOT NULL, 
   last_name VARCHAR2(50) NOT NULL,
   birthdate DATE,
   nationality VARCHAR2(50) NOT NULL,
   filmography CLOB
+);
+
+CREATE TABLE Director (
+  director_id NUMBER PRIMARY KEY, 
+  first_name VARCHAR2(50) NOT NULL, 
+  last_name VARCHAR2(50) NOT NULL,
+  birthdate DATE,
+  nationality VARCHAR2(50) NOT NULL,
+  filmography CLOB
+);
+
+CREATE TABLE Producer (
+  producer_id NUMBER PRIMARY KEY,
+  first_name VARCHAR2(50) NOT NULL, 
+  last_name VARCHAR2(50) NOT NULL,
+  birthdate DATE,
+  nationality VARCHAR2(50) NOT NULL,
+  filmography CLOB
+);
+
+CREATE TABLE User (
+  user_id NUMBER PRIMARY KEY, 
+  first_name VARCHAR2(50) NOT NULL, 
+  last_name VARCHAR2(50) NOT NULL, 
+  username VARCHAR2(50) UNIQUE NOT NULL,
+  email VARCHAR2(100) UNIQUE NOT NULL, 
+  password VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE Awards (
+  award_id NUMBER PRIMARY KEY, 
+  name VARCHAR2(100) NOT NULL,
+  presenter VARCHAR2(100) NOT NULL,
+  year NUMBER,
+  winner_id NUMBER, 
+  winner_type VARCHAR2(20) CHECK (winner_type IN ('Person', 'Film')),
+  FOREIGN KEY (winner_id, winner_type) REFERENCES PersonFilm(person_film_id, person_film_type)
 );
