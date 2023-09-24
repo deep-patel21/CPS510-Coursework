c57f472dd5bbe770272f501fb9a0b1891f08416c
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-09-24 18:28:32 -0400

    Update Sec09_A17_Combined_SQL.sql

--- Sec09_A17_Combined_SQL.sql
+++ Sec09_A17_Combined_SQL.sql
@@ -8,7 +8,7 @@ CREATE TABLE Film (
   release_year NUMBER, 
   director_id NUMBER,
   FOREIGN KEY (director_id) REFERENCES Director(director_id),
-  cast VARCHAR2(1000)
+  cast VARCHAR2(1000) NOT NULL
 );
 
 CREATE TABLE Actor (
@@ -38,13 +38,32 @@ CREATE TABLE Producer (
   filmography CLOB
 );
 
-CREATE TABLE User (
+CREATE TABLE TheUser (
   user_id NUMBER PRIMARY KEY, 
   first_name VARCHAR2(50) NOT NULL, 
   last_name VARCHAR2(50) NOT NULL, 
   username VARCHAR2(50) UNIQUE NOT NULL,
   email VARCHAR2(100) UNIQUE NOT NULL, 
   password VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE Review (
+  review_id NUMBER PRIMARY KEY, 
+  used_id NUMBER, 
+  film_id NUMBER, 
+  description VARCHAR2(500),
+  rating NUMBER, 
+  date DATE,
+  FOREIGN KEY (user_id) REFERENCES User(user_id),
+  FOREIGN KEY (film_id) REFERENCES Film(film_id)
+);
+
+CREATE TABLE Studio (
+  studio_id NUMBER PRIMARY KEY,
+  name VARCHAR2(100) NOT NULL,
+  owner VARCHAR2(50),
+  location VARCHAR2(200),
+  credits VARCHAR2(800)
 );
 
 CREATE TABLE Awards (
@@ -56,7 +75,6 @@ CREATE TABLE Awards (
   winner_type VARCHAR2(20) CHECK (winner_type IN ('Person', 'Film')),
   FOREIGN KEY (winner_id, winner_type) REFERENCES PersonFilm(person_film_id, person_film_type)
 );
-
 ------------------------------------
 -- Create Tables of Relationships --
 ------------------------------------
