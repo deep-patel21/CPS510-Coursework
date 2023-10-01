3b7ff3fdb7efc97e473505f13e7277e3a3fc194e
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-10-01 16:01:16 -0400

    Update Sec09_T17_A04_SimpleQueries.sql

--- Lab 4.1 - Simple Queries/Sec09_T17_A04_SimpleQueries.sql
+++ Lab 4.1 - Simple Queries/Sec09_T17_A04_SimpleQueries.sql
@@ -2,33 +2,46 @@
 -- Sample SELECT Queries --
 ---------------------------
 
-SELECT *
-FROM Film;
+-- Associated with FILM TABLE
+-- Expected Output: Oppenheimer
+SELECT f.title, f.runtime AS 'runtime (minutes)', CONCAT(d.first_name, ' ', d.last_name) AS director_name
+FROM Film f
+JOIN Director d ON f.director_id = d.director_id
 
-SELECT title, runtime, release_year
-FROM Film
-WHERE runtime >= 170;
-
-SELECT title
-From Film
-WHERE director_id = 1;
-
-SELECT first_name, last_name
+-- Associated with ACTOR TABLE
+-- Expected Output: {Robert}, {Downey Jr.}, {American}
+--                  {Matt}, {Damon}, {American}
+--                  {Linda}, {Hamilton}, {American}
+--                  {Edward}, {Furlong}, {American}
+--                  {Tom}, {Cruise}, {American}
+--                  {Jeremy}, {Renner}, {American}
+SELECT first_name, last_name, nationality
 FROM Actor
 WHERE nationality = 'American';
 
-SELECT name
-FROM Studio
-WHERE location = 'Burbank, California';
+-- Associated with DIRECTOR TABLE
+-- Expected Output: {James}, {Cameron}, {1954}, {Titanic, Avatar: The Way of Water, Terminator 2: Judgement Day}
+--                  {Brad}, {Bird}, {1957}, {The Incredibles, Ratatouille, Cars}
+SELECT first_name, last_name, birthdate, filmography
+FROM Director
+WHERE EXCTRACT(YEAR FROM birthdate) < 1960
 
-SELECT rating
-FROM Review
-WHERE rating = 4;
+-- Associated with PRODUCER TABLE
 
-SELECT email
-FROM TheUser;
 
-SELECT receiver_id, theName, year_of_win
-FROM Awards
-WHERE year_of_win = '2023';
- 
+-- Associated with THEUSER TABLE
+
+
+-- Associated with REVIEW TABLE
+
+
+-- Associated with STUDIO TABLE
+
+
+-- Associated with AWRADS TABLE
+
+
+-- Associated with RECIEVER TABLE
+
+
+
