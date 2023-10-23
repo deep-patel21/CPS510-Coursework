f9e8e5daecf7c3f5c725c6604ae99017e53191c8
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-10-22 21:25:01 -0400

    Update drop_tables.sh

--- Lab 5 - UNIX Shell Implementation/drop_tables.sh
+++ Lab 5 - UNIX Shell Implementation/drop_tables.sh
@@ -1,16 +1,22 @@
 
 #!/bin/bash
-
-# Define your Oracle Database connection information
-DB_USER = "dcpatel"
-DB_PASS = "null"
-DB_HOST = "oracle.scs.ryerson.ca"
-DB_SID = "orcl"
-
-# Define the SQL command to drop tables
-SQL_COMMAND="DROP TABLE your_table_name;"
-
-# Execute the SQL command using sqlplus
-sqlplus -s ${DB_USER}/${DB_PASS}@${DB_HOST}/${DB_SID} <<EOF
-$SQL_COMMAND
+#export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+sqlplus64 "dcpatel/06210050@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+DROP TABLE Actor CASCADE CONSTRAINTS;
+DROP TABLE Director CASCADE CONSTRAINTS;
+DROP TABLE Producer CASCADE CONSTRAINTS;
+DROP TABLE TheUser CASCADE CONSTRAINTS;
+DROP TABLE Studio CASCADE CONSTRAINTS;
+DROP TABLE Receiver CASCADE CONSTRAINTS;
+DROP TABLE Film CASCADE CONSTRAINTS;
+DROP TABLE Review CASCADE CONSTRAINTS;
+DROP TABLE Awards CASCADE CONSTRAINTS;
+DROP TABLE Actor_Acts_In_Film CASCADE CONSTRAINTS;
+DROP TABLE Director_Directs_Film CASCADE CONSTRAINTS;
+DROP TABLE Producer_Produces_Film CASCADE CONSTRAINTS;
+DROP TABLE Studio_Owns_Film CASCADE CONSTRAINTS;
+DROP TABLE Awards_Given_To_Film CASCADE CONSTRAINTS;
+DROP TABLE TheUser_Creates_Review CASCADE CONSTRAINTS;
+DROP TABLE Review_Rates_Film CASCADE CONSTRAINTS;
+exit;
 EOF
