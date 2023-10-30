4aa53dc147249f7da8d01580591737b55c079916
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-10-30 15:17:13 -0400

    Update Sec09_T17_Combined_SQL.sql

--- Sec09_T17_Combined_SQL.sql
+++ Sec09_T17_Combined_SQL.sql
@@ -172,6 +172,23 @@ VALUES (4, 'Harry Potter and The Prizoner of Azkaban', 139, 2004, 4, 'Daniel Rad
 INSERT INTO Film(film_id, title, runtime, release_year, director_id, theCast)
 VALUES (5, 'Dunkirk', 106, 2017, 1, 'Tom Hardy, Cillian Murphy, Harry Styles');
 
+INSERT INTO Film(film_id, title, runtime, release_year, director_id, theCast)
+VALUES (6, 'Pulp Fiction', 154, 1994, 5, 'John Travolta, Samuel L. Jackson, Uma Thurman');
+
+INSERT INTO Film(film_id, title, runtime, release_year, director_id, theCast)
+VALUES (7, 'Jurassic Park', 127, 1993, 6, 'Sam Neill, Laura Dern, Jeff Goldblum');
+
+INSERT INTO Film(film_id, title, runtime, release_year, director_id, theCast)
+VALUES (8, 'The Matrix', 136, 1999, 8, 'Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss');
+
+INSERT INTO Film(film_id, title, runtime, release_year, director_id, theCast)
+VALUES (9, 'Forrest Gump', 142, 1994, 9, 'Tom Hanks, Robin Wright, Gary Sinise');
+
+INSERT INTO Film(film_id, title, runtime, release_year, director_id, theCast)
+VALUES (10, 'The Shawshank Redemption', 142, 1994, 10, 'Tim Robbins, Morgan Freeman, Bob Gunton');
+
+
+
 -- Insert Details into Actor Table
 INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
 VALUES (1, 'Cillian', 'Murphy', TO_DATE('1976-05-25', 'YYYY-MM-DD'), 'Irish', '28 Days Later, The Dark Knight, Oppenheimer');
@@ -200,6 +217,17 @@ VALUES (8, 'Jeremy', 'Renner', TO_DATE('1971-01-07', 'YYYY-MM-DD'), 'American', 
 INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
 VALUES (9, 'Simon', 'Pegg', TO_DATE('1970-02-14', 'YYYY-MM-DD'), 'British', 'The Ice Age Adventures of Buck Wild, Hot Fuzz, Mission: Impossible - Dead Reckoning Part One');
 
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (10, 'Meryl', 'Streep', TO_DATE('1949-06-22', 'YYYY-MM-DD'), 'American', 'The Devil Wears Prada, Mamma Mia!, The Iron Lady');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (11, 'Leonardo', 'DiCaprio', TO_DATE('1974-11-11', 'YYYY-MM-DD'), 'American', 'Titanic, The Revenant, Inception');
+
+INSERT INTO Actor (actor_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (12, 'Angelina', 'Jolie', TO_DATE('1975-06-04', 'YYYY-MM-DD'), 'American', 'Tomb Raider, Maleficent, Girl, Interrupted');
+
+
+
 
 -- Insert Details into Director Table
 INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
@@ -214,6 +242,22 @@ VALUES (3, 'Brad', 'Bird', TO_DATE('1957-09-24', 'YYYY-MM-DD'), 'American', 'The
 INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
 VALUES (4, 'Alfonso', 'Cuaron', TO_DATE('1961-11-08', 'YYYY-MM-DD'), 'Mexican', 'Gravity, Roma, A Little Princess');
 
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (5, 'Quentin', 'Tarantino', TO_DATE('1963-03-27', 'YYYY-MM-DD'), 'American', 'Pulp Fiction, Kill Bill, Django Unchained');
+
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (6, 'Steven', 'Spielberg', TO_DATE('1946-12-18', 'YYYY-MM-DD'), 'American', 'Jurassic Park, E.T. the Extra-Terrestrial, Schindler''s List');
+
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (7, 'Lana', 'Wachowski', TO_DATE('1965-06-21', 'YYYY-MM-DD'), 'American', 'The Matrix, Cloud Atlas, Sense8');
+
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (8, 'Robert', 'Zemeckis', TO_DATE('1951-05-14', 'YYYY-MM-DD'), 'American', 'Forrest Gump, Back to the Future, Cast Away');
+
+INSERT INTO Director (director_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (9, 'Frank', 'Darabont', TO_DATE('1959-01-28', 'YYYY-MM-DD'), 'American', 'The Shawshank Redemption, The Green Mile, The Mist');
+
+
 -- Insert Details into Producer Table
 INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
 VALUES (1, 'Emma', 'Thomas', TO_DATE('1971-12-09', 'YYYY-MM-DD'), 'British', 'Inception, Interstellar, Dunkirk');
@@ -223,6 +267,16 @@ VALUES (2, 'Edward', 'Furlong', TO_DATE('1977-08-02', 'YYYY-MM-DD'), 'American',
 
 INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
 VALUES (3, 'Bryan', 'Burk', TO_DATE('1968-12-30', 'YYYY-MM-DD'), 'American', 'Mission: Impossible - Rogue Nation, Star Trek Into Darkness, The Cloverfield Paradox');
+
+INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (4, 'Kathleen', 'Kennedy', TO_DATE('1953-06-05', 'YYYY-MM-DD'), 'American', 'Jurassic Park, E.T. the Extra-Terrestrial, Star Wars: The Force Awakens');
+
+INSERT INTO Producer (producer_id, first_name, last_name, birthdate, nationality, filmography)
+VALUES (5, 'Jerry', 'Bruckheimer', TO_DATE('1945-09-21', 'YYYY-MM-DD'), 'American', 'Pirates of the Caribbean, Top Gun, Armageddon');
+
+
+
+
 
 -- Insert Details into User Table
 INSERT INTO TheUser(user_id, first_name, last_name, username, email, password)
@@ -237,6 +291,16 @@ VALUES (3, 'Aryan', 'Patel', 'Rayna', 'aryan.patel@torontomu.ca', 'Admin3');
 INSERT INTO TheUser(user_id, first_name, last_name, username, email, password)
 VALUES (4, 'John', 'Doe', 'JohnDoe123', 'john.doe@gmail.com', 'ilovemovies');
 
+INSERT INTO TheUser(user_id, first_name, last_name, username, email, password)
+VALUES (5, 'Emma', 'Smithson', 'harrypotterfan450', 'emma.smithson@gmail.com', 'voldemortthedark');
+
+INSERT INTO TheUser(user_id, first_name, last_name, username, email, password)
+VALUES (6, 'Daniel', 'Mosfet', '007Lover', 'daniel.mosfet@007.com', '007SecretAgent');
+
+INSERT INTO TheUser(user_id, first_name, last_name, username, email, password)
+VALUES (7, 'System', 'Admin', 'AdminSir', 'sytem_admin@dbms.com', 'AdminMaster');
+
+
 -- Insert Details into Studio Tables
 INSERT INTO Studio(studio_id, name, owner, location, credits)
 VALUES (1, 'Universal Studios', 'Mark Woodbury', 'Universal City, California', 'Fast X, The Super Mario Bros, Jurrasic World: Dominion');
@@ -246,6 +310,17 @@ VALUES (2, 'Warner Bros', 'David Zaslav', 'Burbank, California', 'Tenet, Incepti
 
 INSERT INTO Studio(studio_id, name, owner, location, credits)
 VALUES (3, 'Paramount Pictures', 'Brian Robbins', 'Los Angeles, California', 'Scream, Shrek Forever After, Top Gun: Maverick');
+
+INSERT INTO Studio(studio_id, name, owner, location, credits)
+VALUES (4, 'Sony Pictures Entertainment', 'Tony Vinciquerra', 'Culver City, California', 'Spider-Man: No Way Home, Venom: Let There Be Carnage, Ghostbusters: Afterlife');
+
+INSERT INTO Studio(studio_id, name, owner, location, credits)
+VALUES (5, '20th Century Studios', 'Dana Walden', 'Century City, California', 'Deadpool, The Martian, Logan');
+
+INSERT INTO Studio(studio_id, name, owner, location, credits)
+VALUES (6, 'New Line Cinema', 'Ann Sarnoff', 'Burbank, California', 'The Lord of the Rings: The Fellowship of the Ring, A Nightmare on Elm Street, The Conjuring');
+
+
 
 -- Insert Details into Awards Tables
 INSERT INTO Awards(award_id, theName, presenter, year_of_win, receiver_id, winner_type)
@@ -257,6 +332,14 @@ VALUES (2, 'Best Actor', 'Oscars Ceremony', '2023', 2, 'Person');
 INSERT INTO Awards(award_id, theName, presenter, year_of_win, receiver_id, winner_type)
 VALUES (3, 'Best Director', 'Academy Awards', '2014', 3, 'Person');
 
+INSERT INTO Awards(award_id, theName, presenter, year_of_win, receiver_id, winner_type)
+VALUES (4, 'Best Actress', 'Academy Awards', '2018', 2, 'Person');
+
+INSERT INTO Awards(award_id, theName, presenter, year_of_win, receiver_id, winner_type)
+VALUES (5, 'Best Original Score', 'Grammy Awards', '2022', 1, 'Film');
+
+
+
 -- Insert Details into Review Table
 INSERT INTO Review(review_id, user_id, film_id, theDescription, rating, theDate)
 VALUES (1, 3, 2, 'This movie was action packed with my favorite action hero Arnold!', 4, TO_DATE('2023-07-03', 'YYYY-MM-DD'));
@@ -266,6 +349,14 @@ VALUES (2, 2, 3, 'The iconic run of Tom Cruise made this movie epic to watch.', 
 
 INSERT INTO Review(review_id, user_id, film_id, theDescription, rating, theDate)
 VALUES (3, 1, 1, 'Amazing cinematics from Nolan once again. Pristine performance!.', 5, TO_DATE('2023-06-21', 'YYYY-MM-DD'));
+
+INSERT INTO Review(review_id, user_id, film_id, theDescription, rating, theDate)
+VALUES (4, 4, 6, 'An unforgettable cinematic experience! Tarantino''s masterpiece.', 5, TO_DATE('2023-09-12', 'YYYY-MM-DD'));
+
+INSERT INTO Review(review_id, user_id, film_id, theDescription, rating, theDate)
+VALUES (5, 5, 7, 'A thrilling adventure with dinosaurs! Spielberg''s classic.', 4, TO_DATE('2023-08-27', 'YYYY-MM-DD'));
+
+
 
 -- Insert Details into Receiver Table
 INSERT INTO Receiver(receiver_id, theName, theDate)
@@ -279,6 +370,13 @@ VALUES (3, 'Dwayne Johnson', TO_DATE('2012-01-11', 'YYYY-MM-DD'));
 
 INSERT INTO Receiver(receiver_id, theName, theDate)
 VALUES (4, 'Interstellar', TO_DATE('2023-02-17', 'YYYY-MM-DD'));
+
+INSERT INTO Receiver(receiver_id, theName, theDate)
+VALUES (5, 'Inception', TO_DATE('2010-07-08', 'YYYY-MM-DD'));
+
+INSERT INTO Receiver(receiver_id, theName, theDate)
+VALUES (6, 'Jodie Foster', TO_DATE('2013-03-02', 'YYYY-MM-DD'));
+
 
 ---------------------------
 -- Simple SELECT Queries --
