20d39964e64b3b489318428b57df4e0a846701dd
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-11-05 14:37:37 -0500

    Update queries.sh

--- Lab 5 - UNIX Shell Implementation/queries.sh
+++ Lab 5 - UNIX Shell Implementation/queries.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
-sqlplus64 "dcpatel/06210050@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
 set lines 150
 set pagesize 50000
