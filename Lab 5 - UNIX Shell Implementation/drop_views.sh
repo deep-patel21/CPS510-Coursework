ed84c15fdba333d0c90ed622b82849c00b7af2ef
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-11-05 14:37:03 -0500

    Update drop_views.sh

--- Lab 5 - UNIX Shell Implementation/drop_views.sh
+++ Lab 5 - UNIX Shell Implementation/drop_views.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
-sqlplus64 "dcpatel/06210050@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
 DROP VIEW Film_View_without_Director;
 DROP VIEW Actor_Notable_Works;
