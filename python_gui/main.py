32cf01d74f86378368867642e604788255a1074f
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-11-11 12:27:30 -0500

    Delete python_gui directory

--- python_gui/main.py
+++
@@ -1,7 +0,0 @@
-import cx_Oracle
-from Frontend.app import FRONTEND
-
-if __name__ == "__main__":
-    cx_Oracle.init_oracle_client(lib_dir="d:\instantclient_21_12")
-    Frontend = FRONTEND()
-    Frontend.createMainWindow()
